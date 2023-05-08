df134646f8f051220339026b0380fdcaac189a39
Devontef213 <devontef213@gmail.com>
2023-05-08 11:30:04 -0400

    failed cat

--- src/MainScene.js
+++ src/MainScene.js
@@ -1,4 +1,6 @@
 import Phaser from "phaser";
+import 'phaser-matter-collision-plugin';
+
 // import Player from "./Player.js"; ** this is for when the sprites are ready
 
 export default class MainScene extends Phaser.Scene{
@@ -7,21 +9,23 @@ export default class MainScene extends Phaser.Scene{
   }
   preload ()
   {
+    this.load.atlas("cat", "assets/images/cat.png", 'assets/images/cat_atlas');
       console.log('preload')
-      // Player.load(this) ** this is for when the sprites are ready
+      
       // this.load.image('tiles', 'src/assets/images/???')
       // this.load.tilemapTiledJSON('map1', 'src/assets/images/map1.json')
   }
 
   create ()
   {
+    var mySprites = this.add.sprite(300,200,"cat")
     // const map1= this.make.tilemap({key: 'map'})
     // const tileset = map1.addTilesetImage('name', 'tiles', 32,32,0,0)
     // const layer1 = map1.createStaticLayer('Tile layer 1', tileset)
     // layer1.setCollisionByProperty({collides:true})
       console.log('create')
       // this.player = new Player({scene: this, x:0 ,y:0 , texture:'hero',frame:'idle'}) ** this is for when the sprites are ready
-      this.player = new Phaser.Physics.Matter.Sprite(this.matter.world)
+      this.player = new Phaser.Physics.Matter.Sprite(this.matter.world,0,0,'cat', 'cat')
       this.inputKeys = this.input.keyboard.addKeys({
         up: Phaser.Input.Keyboard.KeyCodes.W,
         down: Phaser.Input.Keyboard.KeyCodes.S,
