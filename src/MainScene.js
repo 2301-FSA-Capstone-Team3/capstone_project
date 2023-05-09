501efb144966ff26a5c9c83ebfa9ecc506d09c74
Aaron Sprayberry <aaronsprayberry@icloud.com>
2023-05-09 12:46:07 -0500

    setup for new player and moved lines to player.js

--- src/MainScene.js
+++ src/MainScene.js
@@ -1,57 +1,41 @@
-import Phaser from "phaser";
-import 'phaser-matter-collision-plugin';
+import Player from "./Player.js";
 
-// import Player from "./Player.js"; ** this is for when the sprites are ready
-
-export default class MainScene extends Phaser.Scene{
-  constructor(){
-    super('MainScene');
+export default class MainScene extends Phaser.Scene {
+  constructor() {
+    super("MainScene");
   }
-  preload ()
-  {
-    this.load.atlas("cat", "assets/images/cat.png", 'assets/images/cat_atlas.json');
-    this.load.animation("cat", "assets/images/cat");
-      console.log('preload')
-      
-      // this.load.image('tiles', 'src/assets/images/???')
-      // this.load.tilemapTiledJSON('map1', 'src/assets/images/map1.json')
+  preload() {
+    Player.preload(this);
+
+    //     // this.load.image('tiles', 'src/assets/images/???')
+    //     // this.load.tilemapTiledJSON('map1', 'src/assets/images/map1.json')
   }
 
-  create ()
-  {
-    let mySprites = this.add.sprite(300,200,"cat")
-    // const map1= this.make.tilemap({key: 'map'})
-    // const tileset = map1.addTilesetImage('name', 'tiles', 32,32,0,0)
-    // const layer1 = map1.createStaticLayer('Tile layer 1', tileset)
-    // layer1.setCollisionByProperty({collides:true})
-      console.log('create')
-      // this.player = new Player({scene: this, x:0 ,y:0 , texture:'hero',frame:'idle'}) 
-      this.player = new Phaser.Physics.Matter.Sprite(this.matter.world,0,0,'cat', 'cat_idel')
-      this.inputKeys = this.input.keyboard.addKeys({
-        up: Phaser.Input.Keyboard.KeyCodes.W,
-        down: Phaser.Input.Keyboard.KeyCodes.S,
-        left: Phaser.Input.Keyboard.KeyCodes.A,
-        right: Phaser.Input.Keyboard.KeyCodes.D,
-      })
+  create() {
+    //     let mySprites = this.add.sprite(300, 200, "cat");
+    //     // const map1= this.make.tilemap({key: 'map'})
+    //     // const tileset = map1.addTilesetImage('name', 'tiles', 32,32,0,0)
+    //     // const layer1 = map1.createStaticLayer('Tile layer 1', tileset)
+    //     // layer1.setCollisionByProperty({collides:true})
+    console.log("create");
+    //     // this.player = new Player({scene: this, x:0 ,y:0 , texture:'hero',frame:'idle'})
+    this.player = new Player({
+      scene: this,
+      x: 0,
+      y: 0,
+      texture: "male_main_character",
+      frame: "tile000",
+    });
+    this.add.existing(this.player);
+    this.player.inputKeys = this.input.keyboard.addKeys({
+      up: Phaser.Input.Keyboard.KeyCodes.W,
+      down: Phaser.Input.Keyboard.KeyCodes.S,
+      left: Phaser.Input.Keyboard.KeyCodes.A,
+      right: Phaser.Input.Keyboard.KeyCodes.D,
+    });
   }
-  update ()
-  {
-      // this.player.update() ** this is for when the sprites are ready
-      const speed = 2.5;
-      let playerVelocity = new Phaser.Math.Vector2();
-      if(this.inputKeys.left.isDown){
-        playerVelocity.x = -1
-      }else if (this.inputKeys.right.isDown){
-        playerVelocity.x = 1
-      }
-      if(this.inputKeys.up.isDown){
-        playerVelocity.y = -1
-      }else if (this.inputKeys.down.isDown){
-        playerVelocity.y = 1
-      }
-      playerVelocity.normalize()
-      playerVelocity.scale(speed)
-      this.player.setVelocity(playerVelocity.x, playerVelocity.y)
 
+  update() {
+    this.player.update();
   }
 }
