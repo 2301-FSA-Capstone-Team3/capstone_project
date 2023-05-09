49a4e001a151f5c80b65fa926dd55ed8ef88bb1f
Aaron Sprayberry <aaronsprayberry@icloud.com>
2023-05-09 12:44:58 -0500

    cleaned up format and adjusted imports

--- src/index.js
+++ src/index.js
@@ -1,31 +1,32 @@
-import Phaser from 'phaser';
-import MainScene from './MainScene.js';
-import { PhaserMatterCollisionPlugin } from "phaser-matter-collision-plugin";
+import PhaserMatterCollisionPlugin from "phaser-matter-collision-plugin";
+import MainScene from "./MainScene.js";
+
 const config = {
-    width: 800,
-    height: 600,
-    backgroundColor: '#777777',
-    type: Phaser.AUTO,
-    parent: 'phaser-example',
-    scene: [MainScene],
-    scale:{
-        zoom:2,
+  width: 1600,
+  height: 900,
+  backgroundColor: "#333333",
+  type: Phaser.AUTO,
+  parent: "survival-game",
+  scene: [MainScene],
+  scale: {
+    zoom: 2,
+  },
+  physics: {
+    default: "matter",
+    matter: {
+      debug: true,
+      gravity: { y: 0 },
     },
-    physics:{
-        default: 'matter',
-        matter:{
-            debug: true,
-            gravity: {y:0},
-        }
-    },
-    plugins: {
-        scene: [
-          {
-            plugin: PhaserMatterCollisionPlugin, // The plugin class
-            key: "matterCollision", // Where to store in Scene.Systems, e.g. scene.sys.matterCollision
-            mapping: "matterCollision" // Where to store in the Scene, e.g. scene.matterCollision
-          }
-        ]
-      }
+  },
+  plugins: {
+    scene: [
+      {
+        plugin: PhaserMatterCollisionPlugin,
+        key: "matterCollision",
+        mapping: "matterCollision",
+      },
+    ],
+  },
 };
+
 new Phaser.Game(config);
