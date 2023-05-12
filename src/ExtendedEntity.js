4203c998b3d011dd9f7f7c1188d62eb6122393fe
Greg <greg.pounds3@gmail.com>
2023-05-12 11:34:11 -0500

    dont need the preload,create,update we can just extend this class

--- src/ExtendedEntity.js
+++ src/ExtendedEntity.js
@@ -28,47 +28,4 @@ export default class ExtendedEntity extends Phaser.Physics.Matter.Sprite {
       //more features can be stored here
     }
   }
-  static preload(scene) {
-    scene.load.atlas("cat_sprite", mrmPng, mrmAtlasjson);
-    scene.load.animation("cat_sprite_anims", mrmAnims);
-  }
-  create() {}
-  update() {
-
-    const speed = 2.5;
-    let entityDirection = new Phaser.Math.Vector2();
-    if (this.inputKeys.left.isDown) {
-      entityDirection.x = -1;
-    } else if (this.inputKeys.right.isDown) {
-      entityDirection.x = 1;
-    }
-    if (this.inputKeys.up.isDown) {
-      entityDirection.y = -1;
-    } else if (this.inputKeys.down.isDown) {
-      entityDirection.y = 1;
-    }
-    entityDirection.normalize();
-    entityDirection.scale(speed);
-
-    if (entityDirection.x < 0) {
-      this.setFlipX(true);
-    } else if (entityDirection.x > 0) {
-      this.setFlipX(false);
-    }
-
-    this.setVelocity(entityDirection.x, entityDirection.y);
-    if (this.inputKeys.up.isDown && this.inputKeys.right.isDown) {
-      this.anims.play("cat_walk", true);
-    } else if (this.inputKeys.up.isDown) {
-      this.anims.play("cat_walk", true);
-    } else if (this.inputKeys.down.isDown && this.inputKeys.right.isDown) {
-      this.anims.play("cat_walk", true);
-    } else if (this.inputKeys.down.isDown) {
-      this.anims.play("cat_walk", true);
-    } else if (this.inputKeys.left.isDown || this.inputKeys.right.isDown) {
-      this.anims.play("cat_walk", true);
-    } else {
-      this.anims.play("cat_idle", true);
-    }
-  }
 }
